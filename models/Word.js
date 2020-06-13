1eedc8aad2ca86291764185edff79c1439588f90
Juyeon <56671039+ebooome@users.noreply.github.com>
2020-06-13 18:07:47 +0900

    Video Model

---
+++ models/Word.js
@@ -0,0 +1,12 @@
+import mongoose, { mongo } from "mongoose";
+
+const WordSchemna = new mongoose.Schema({
+  text: { type: String, required: "Text is required" },
+  title: { type: String, required: "Title is required" },
+  description: { type: String, required: "Description is required" },
+  views: { type: Number, default: 0 },
+  createAt: { type: Date, default: Date.now },
+});
+
+const model = mongoose.model("Word", WordSchemna);
+export default model;
